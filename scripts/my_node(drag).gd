5cb458dc840eb81ea4cfbc6c824c5b09869bcf68
BigBarfEaterBBE <ajiang9@bostonk12.org>
2025-07-31 14:06:25 -0400

    7/31

--- scripts/my_node(drag).gd
+++ scripts/my_node(drag).gd
@@ -1,13 +1,19 @@
-extends Sprite2D
+extends RigidBody2D
 
 var dragging = false
 var of = Vector2.ZERO
-var is_template = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-
-
+@onready var sprite = $Sprite2D
+@onready var size = sprite.texture.get_size() * sprite.scale
 
 func _process(delta: float) -> void:
+	var top_left = global_position - size/2
+	var bottom_right = global_position + size/2
+	var mouse_pos = get_global_mouse_position()
+	var mouse_within_node = bool(mouse_pos.x >= top_left.x and mouse_pos.x <= bottom_right.x and mouse_pos.y >= top_left.y and mouse_pos.y <= bottom_right.y)
+	if mouse_within_node and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		dragging = true
+	else:
+		dragging = false
 	if dragging:
 		position = get_global_mouse_position() - of
 
